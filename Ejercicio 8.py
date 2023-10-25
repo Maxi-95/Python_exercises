6efe1786604df5b8d90a430741c7bed613335e36
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-10-25 16:11:08 -0300

    Ejercicio 8 Realizado

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -14,3 +14,10 @@
 # print(frase[i])
 
 # El operador [ ] (corchetes) permite obtener un carácter a partir de un string. La posición del carácter se indica entre los corchetes, ya sea ingresando directamente el número, con una variable que contenga un número o con una operación que de como resultado un número. Siempre, el primer carácter de un string estará ubicado en la posición 0.
+
+
+primera_palabra = "derribando"
+
+segunda_palabra = "muros"
+
+print(primera_palabra + " " + segunda_palabra)
