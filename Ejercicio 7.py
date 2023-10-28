348153af6907323aebda7a8b14fdd9ec2cb1498a
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-10-28 10:04:58 -0300

    python

--- Ejercicio 7.py
+++ Ejercicio 7.py
@@ -4,12 +4,3 @@
 
 # Ingresá un número: 260
 # Descontando el 15% queda: 221.0
-
-# Código de ejemplo
-
-# cadena=""
-# cadena=cadena+"buen"
-# cadena=cadena+" día"
-# print(cadena)
-
-# Cuando se utiliza el operador + en una operación entre strings, se está realizando una concatenación (unión de strings). La instrucción print mostrada arriba imprimirá “buen día”.
