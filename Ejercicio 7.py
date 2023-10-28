957b9e51389f320c23ea8a2ac248f054ffbfc598
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-10-28 11:59:47 -0300

    cadenas: ejercicio 1

--- Ejercicio 7.py
+++
@@ -1,6 +0,0 @@
-# Escribí un programa que solicite al usuario un número y le reste el 15%, almacenando todo en una única variable. A continuación, mostrar el resultado final en pantalla.
-
-# Ejemplo de ejecución:
-
-# Ingresá un número: 260
-# Descontando el 15% queda: 221.0
--- Tareas 2/Cadenas.py
+++ Tareas 2/Cadenas.py
@@ -1,6 +1,16 @@
 # Ejercicio 1
 # Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
 
+nombre = input("ingresar nombre: ")
+numero = int(input("ingresa un numero: "))
+
+# inicio = 0
+# while inicio < repeticiones:
+#     print(f"{nombre} dice hola")
+#     inicio += 1
+
+for _ in range(numero):
+    print(nombre)
 
 # Ejercicio 2
 # Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
