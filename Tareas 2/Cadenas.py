7561a9e0f360c9f1cd98fb42394facc7d36e8fa3
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-10-30 21:24:48 -0300

    cadenas: ejercicio 6

--- Tareas 2/Cadenas.py
+++ Tareas 2/Cadenas.py
@@ -29,17 +29,39 @@
 # Ejercicio 3
 # Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número de letras que tienen el nombre.
 
+# nombre = input("ingrese el nombre: ")
+# print(f"el nombre {nombre.upper()} tiene {len(nombre)} caracteres")
+
 # --------------------------------------------------
 # Ejercicio 4
 # Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
+
+# telefono = input("numero de telefono: ")
+# lista = telefono.split()
+# numero = lista[1]
+
+# print(f"Tu numero es: {numero}")
+# # print(lista)
 
 # --------------------------------------------------
 # Ejercicio 5
 # Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
 
+# frase = input("introducir frase: ")
+
+# reversa = frase[::-1]
+# print(reversa)
+
 # --------------------------------------------------
 # Ejercicio 6
 # Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
+
+frase = input("frase por favor: ")
+vocal = input("vocal por favor: ")
+lista = list(frase)
+print(lista)
+
+# for la_vocal in frase:
 
 # --------------------------------------------------
 # Ejercicio 7
