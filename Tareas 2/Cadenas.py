62793886fa86a6dd534f8bc14aed6976fca03c1c
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-10-31 20:46:59 -0300

    cadenas: ejercicio 6

--- Tareas 2/Cadenas.py
+++ Tareas 2/Cadenas.py
@@ -56,20 +56,22 @@
 # Ejercicio 6
 # Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
 
-# frase = input("frase por favor: ")
-# vocal = input("vocal por favor: ")
-# lista = list(frase)
-# print(lista)
+frase = input("frase por favor: ")
+vocal = input("vocal por favor: ")
+lista = list(frase)
+print(lista)
 
-# new_array = []
+new_array = []
 
-# for la_vocal in frase:
-#     if la_vocal == vocal:
-#         vocal = la_vocal.upper()
-#         result = la_vocal.replace(la_vocal, vocal)
-#         new_array.append(result)
-#         print(result)
+for la_vocal in frase:
+    #    if la_vocal == vocal:
+    # vocal = la_vocal.upper()
+    result = la_vocal.replace(vocal, vocal.upper())
+    new_array.append(result)
 
+
+s = "".join(new_array)
+print(s)
 
 # --------------------------------------------------
 # Ejercicio 7
