c68b7ecabf0f72a2747ff7c13124e58974972350
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-10-31 20:33:32 -0300

    cambios pequeños

--- Tareas 2/Cadenas.py
+++ Tareas 2/Cadenas.py
@@ -56,12 +56,20 @@
 # Ejercicio 6
 # Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
 
-frase = input("frase por favor: ")
-vocal = input("vocal por favor: ")
-lista = list(frase)
-print(lista)
+# frase = input("frase por favor: ")
+# vocal = input("vocal por favor: ")
+# lista = list(frase)
+# print(lista)
+
+# new_array = []
 
 # for la_vocal in frase:
+#     if la_vocal == vocal:
+#         vocal = la_vocal.upper()
+#         result = la_vocal.replace(la_vocal, vocal)
+#         new_array.append(result)
+#         print(result)
+
 
 # --------------------------------------------------
 # Ejercicio 7
