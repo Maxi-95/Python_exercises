8607627fc1403068543ad3a9731aa98963238592
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-11-02 16:56:45 -0300

    Exercises: 2/10

--- Tareas 2/Bucles.py
+++ Tareas 2/Bucles.py
@@ -9,23 +9,32 @@
 #     print(palabra)
 #     numero += 1 
 
-
+# --------------------------------------------------
 # Ejercicio 2
 # Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
 
+edad = int(input("ingrese su edad: "))
 
+numero = 0
+while numero <= edad:
+    print(numero)
+    numero += 1
+    
+    
+
+# --------------------------------------------------
 # Ejercicio 3
 # Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
 
-
+# --------------------------------------------------
 # Ejercicio 4
 # Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la cuenta atrás desde ese número hasta cero separados por comas.
 
-
+# --------------------------------------------------
 # Ejercicio 5
 # Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años, y muestre por pantalla el capital obtenido en la inversión cada año que dura la inversión.
 
-
+# --------------------------------------------------
 # Ejercicio 6
 # Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo, de altura el número introducido.
 
@@ -34,11 +43,11 @@
 # ***
 # ****
 # *****
-
+# --------------------------------------------------
 # Ejercicio 7
 # Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
 
-
+# --------------------------------------------------
 # Ejercicio 8
 # Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo.
 
@@ -47,10 +56,10 @@
 # 5 3 1
 # 7 5 3 1
 # 9 7 5 3 1
-
+# --------------------------------------------------
 # Ejercicio 9
 # Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
 
-
+# --------------------------------------------------
 # Ejercicio 10
 # Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no.
