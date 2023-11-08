e7d38c1749a4be056a32058b2fd306577cc12c23
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-11-08 14:13:29 -0300

    Exercises: 5/10

--- Tareas 2/Condicionales.py
+++ Tareas 2/Condicionales.py
@@ -1,23 +1,60 @@
 # Ejercicio 1
 # Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
 
+# edad = int(input("su edad por favor: "))
+
+# if edad < 18:
+#     print("eres menor de edad")
+# else:
+#     print("eres mayor de edad")    
 
 # Ejercicio 2
 # Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
 
+# contraseña = "ContrAseñA"
+# contraseña2 = input("contraseña: ")
+
+
+# if contraseña2.upper() == contraseña.upper():
+#     print("contraseña correcta")
+# else:
+#     print("contraseña incorrecta")
 
 # Ejercicio 3
 # Escribir un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el programa debe mostrar un error.
 
+# numero = int(input("numero a divir: "))
+# divisor = int(input("numero divisor: "))
 
+# result = numero // divisor
+# if result == 0:
+#     print("Error")
+# else: 
+#     print(result)
+    
 # Ejercicio 4
 # Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
 
+# n = int(input("numero: "))
+# if n % 2 == 0: 
+#     print(n, "es par.")
+# else:
+#     print(n, "es impar")
 
 # Ejercicio 5
 # Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
 
+edad = int(input("edad: "))
+sueldo = int(input("sueldo: "))
 
+if edad > 16:
+    if sueldo > 1000:
+        print("tiene que tributar")
+    if sueldo < 1000:
+        print("no gana lo suficiente para tributar")
+else:
+    print("todabia es menor para tributar")
+    
 # Ejercicio 6
 # Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre. El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un nombre posterior a la N y el grupo B por el resto. Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
 
