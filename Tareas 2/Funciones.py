c5f244ea693a5a2d66c583164524b3aa2f223222
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-12-05 11:20:09 -0300

    Exercises: 3/10

--- Tareas 2/Funciones.py
+++ Tareas 2/Funciones.py
@@ -19,6 +19,32 @@
 # Ejercicio 3
 # Escribir una función que reciba un número entero positivo y devuelva su factorial.
 
+# def factorial(num):
+#     inicio = 0
+#     result = 1
+#     while inicio < num:
+#         inicio += 1
+#         result = inicio * result
+
+#     return inicio
+
+
+# result = factorial(3)
+# print(result)
+
+
+# def factorial(num):
+#     # inicio = int(num)
+#     result = 1
+
+#     for i in range(1, num + 1, 1):
+#         result *= i
+
+#     return result
+
+
+# resultado = factorial(5)
+# print(resultado)
 
 # Ejercicio 4
 # Escribir una función que calcule el total de una factura tras aplicarle el IVA. La función debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar, y devolver el total de la factura. Si se invoca la función sin pasarle el porcentaje de IVA, deberá aplicar un 21%.
