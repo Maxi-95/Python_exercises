90cede9416cc55371ba92183563550c1793a6639
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-12-28 14:38:24 -0300

    Exercises: 2/10

--- Tareas 2/Listas y Tuplas.py
+++ Tareas 2/Listas y Tuplas.py
@@ -14,6 +14,12 @@
 # Ejercicio 2
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
 
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+
+# Muestra las asignaturas por pantalla
+print("Asignaturas del curso:")
+for asignatura in asignaturas:
+    print(f"Yo estudio {asignatura}")
 
 # Ejercicio 3
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
