8f9675046727304b9fd7eecff6155e5e6240515a
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-12-28 14:44:18 -0300

    Exercises: 3/10

--- Tareas 2/Listas y Tuplas.py
+++ Tareas 2/Listas y Tuplas.py
@@ -14,16 +14,21 @@
 # Ejercicio 2
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
 
-asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+# asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
 
-# Muestra las asignaturas por pantalla
-print("Asignaturas del curso:")
-for asignatura in asignaturas:
-    print(f"Yo estudio {asignatura}")
+# # Muestra las asignaturas por pantalla
+# print("Asignaturas del curso:")
+# for asignatura in asignaturas:
+#     print(f"Yo estudio {asignatura}")
 
 # Ejercicio 3
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
 
+# asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+
+# for asignatura in asignaturas:
+#     note = input(f"nota de {asignatura}: ")
+#     print(f"{asignatura}: {note}")
 
 # Ejercicio 4
 # Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
